5624a16b6ba0983e4a9ce6796505e0f5771fc88c
stufkan <stufkan@gmail.com>
2014-03-21 00:25:25 +0100

    Created interface for printers

---
+++ RobotGameTournament/Control/IPrintScores.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace RobotGameTournament.Control
+{
+    public interface IPrintScores
+    {
+        void PrintMatchOverview(System.Collections.Generic.List<RobotGameTournament.Match> matches);
+        void PrintScoreBoard(System.Collections.Generic.List<RobotGameTournament.Model.Robot> robots);
+    }
+}
---
+++ RobotGameTournament/Control/PrintScoresToConsole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotGameTournament.Model;
+
+namespace RobotGameTournament
+{
+    public class PrintScoresToConsole : RobotGameTournament.Control.IPrintScores
+    {
+        public void PrintScoreBoard(List<Robot> robots)
+        {
+            foreach (Robot r in robots)
+            {
+                Console.WriteLine(r.ShortName + ": " + r.Points);
+            }
+        }
+
+        public void PrintMatchOverview(List<Match> matches)
+        {
+            foreach (Match m in matches)
+            {
+                string match = string.Format("{0} {1} \t vs. \t {3} {2}", m.Player1, m.MatchResult.player1score, m.Player2, m.MatchResult.player2score);
+                if (m.Winner() == Robot.Draw())
+                    Console.WriteLine(match + " - Draw!");
+                else Console.WriteLine(match+ string.Format(" - {0} wins!", m.Winner()));
+            }
+        }
+    }
+}
