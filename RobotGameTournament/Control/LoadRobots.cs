a46ee08252e35cdc753a74f6015a7c3fc70e7b48
stufkan <stufkan@gmail.com>
2014-03-21 00:25:44 +0100

    createt class for loading robots

---
+++ RobotGameTournament/Control/LoadRobots.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotGameTournament.Model;
+
+namespace RobotGameTournament.Control
+{
+    public static class LoadRobots
+    {
+        public  static List<Robot> GetRobots(string dir)
+        {
+            string[] files = Directory.GetFiles(dir);
+
+            List<Robot> robots = new List<Robot>();
+            foreach (var item in files)
+            {
+                if (item.Split('.')[1] == "py")
+                    robots.Add(new Robot(item));
+            }
+            return robots;
+        }
+    }
+}
