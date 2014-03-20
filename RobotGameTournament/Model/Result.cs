7320dcce2909ab19aaaa5467002ee0dcf938e38e
stufkan <stufkan@gmail.com>
2014-03-21 00:30:09 +0100

    created Result, a class for describing the result of a match

---
+++ RobotGameTournament/Model/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotGameTournament.Model
+{
+    public enum Result { Player1, Player2, Draw }
+    public struct MatchResult
+    {
+        public int player1score;
+        public int player2score;
+        public Result result;
+
+        public MatchResult(int player1score, int player2score, Result res)
+        {
+            this.player1score = player1score;
+            this.player2score = player2score;
+            result = res;
+        }
+
+
+    }
+}
